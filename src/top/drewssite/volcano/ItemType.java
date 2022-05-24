c4fc5d073e48f829a09add391e9b5d3e56760366
foxler2010 <drew@drewssite.top>
2022-05-24 18:57:20 -0500

    clarify a comment

--- src/top/drewssite/volcano/ItemType.java
+++ src/top/drewssite/volcano/ItemType.java
@@ -1,7 +1,7 @@
 package top.drewssite.volcano;
 
 //this is one way to find out what an item 'is'.
-//combined with the inheritance and abstraction of items, categorization should be very easy
+//combined with the inheritance structure of items, categorization should be very easy
 public enum ItemType {
 	//below could be considered a roadmap of all types of items that are,
 	//or will be added to the game
